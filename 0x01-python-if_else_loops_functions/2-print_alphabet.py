3593fcc1186771ffbae6e0b0237f32a52e38543f
EscoBaite <mwitiben@gmail.com>
2022-06-01 19:25:07 +0300

    Another First task

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for letter in range(26):
-    print("{:s}".format(chr(letter + ord("a"))), end="")
+for a in range(ord('a'), ord('z') + 1):
+    print("{:c}".format(a), end='')
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for letter in range(26):
-    if letter != 4 and letter != 16:
-        print("{:s}".format(chr(letter + ord("a"))), end="")
+for char in range(26):
+    if char != 4 and char != 16:
+        print("{:s}".format(chr(char + ord("a"))), end="")
