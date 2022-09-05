76dee4e453f319dc92b8acac1ce326131864ba4c
EscoBaite <mwitiben@gmail.com>
2022-09-06 01:48:02 +0300

    Sprint 3

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+/**
+ * Script that computes and prints factorials
+ */
+function factorial (num) {
+  if (num === 1 || num === 1 || isNaN(num)) {
+    return (1);
+  }
+  return (factorial(num - 1) * num);
+}
+console.log(factorial(parseInt(process.argv[2])));
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < myArray.length; i++) {
+  console.log(myArray[i]);
+}
